aa68a854307bc02818cfdc9b1692a061b885bcc7
fdj <2089045617@qq.com>
2019-07-01 10:33:22 +0800

    Merge branch 'master' of D:\project\homework_system with conflicts.

---
+++ src/test/java/gxun/soft/homework_system/HomeworkSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package gxun.soft.homework_system;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HomeworkSystemApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
