f0186a30e34a2208eda7badcca9aef8bda1b13cb
fdj <2089045617@qq.com>
2019-07-01 10:27:05 +0800

    添加swagger2

--- src/main/java/gxun/soft/homework_system/controller/account.java
+++ src/main/java/gxun/soft/homework_system/controller/account.java
@@ -1,10 +1,16 @@
 package gxun.soft.homework_system.controller;
 
+import io.swagger.annotations.Api;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@Api(value = "账号管理")
+@RestController
+@RequestMapping("/v1")
 public class account {
+
+
     @RequestMapping("/login")
     public String suc(){
         return "success";
