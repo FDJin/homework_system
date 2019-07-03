cd299d7fb5c1500e72a74c97259e671182def434
fdj <2089045617@qq.com>
2019-07-04 00:52:37 +0800

    test push

--- src/main/java/gxun/soft/homework_system/controller/config/MyMvcConfig.java
+++ src/main/java/gxun/soft/homework_system/controller/config/MyMvcConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class MyMvcConfig implements WebMvcConfigurer {
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry){
         registry.addViewController("/").setViewName("index");
