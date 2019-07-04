0ea28d7430fd17031ed32805e4d3fef6bf5651bd
fengjie <1042048190@qq.com>
2019-07-05 02:53:32 +0800

    QuestionServiceImp

--- src/main/java/gxun/soft/homework_system/service/impl/QuestionServiceImp.java
+++ src/main/java/gxun/soft/homework_system/service/impl/QuestionServiceImp.java
@@ -33,5 +33,6 @@ public class QuestionServiceImp implements QuestionService {
     @Override
     public List<Question> getAllQuestions() {
         return questionMapper.getAllQuestions();
+
     }
 }
