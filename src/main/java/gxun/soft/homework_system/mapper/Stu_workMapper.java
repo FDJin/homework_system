a47a924a5a0226e54fb29ebb5030b2447c38e5ec
AronWiki <1639563006@qq.com>
2019-07-04 20:12:12 +0800

    更新mapper接口

--- src/main/java/gxun/soft/homework_system/mapper/Stu_workMapper.java
+++ src/main/java/gxun/soft/homework_system/mapper/Stu_workMapper.java
@@ -12,21 +12,37 @@ public interface Stu_workMapper {
 
     //学生答题情况操作
 
+    /**
+     * 新增答题记录
+     * @param stu_work
+     * @return
+     */
     int addStu_Work(@Param("stu_work") Stu_work stu_work);
 
-    int deleteStu_workById(@Param("stu_workId") Integer stu_workId);
-
+    /**
+     * 更新答题记录,可更新 分数、答题状态、答题答案
+     * @param stu_work
+     * @return
+     */
     int updateStu_work(@Param("Stu_work") Stu_work stu_work);
 
-    //以stu_workId查找学生答题记录
-    Stu_work findStu_workByStu_workId(@Param("stu_workId") Integer stu_workId);
-
-    //以workId查找学生答题记录
+    /**
+     *以workId查找学生答题记录
+     * @param workId
+     * @return
+     */
     List<Stu_work> findStu_workByWorkId(@Param("workId") Integer workId);
 
-    //以stuId查找学生答题记录
+    /**
+     * 按stuId查找答题记录
+     * @param stuId
+     * @return
+     */
     List<Stu_work> findStuWorkByStuId(@Param("stuId") Integer stuId);
 
-    //获取全部学生答题记录
+    /**
+     * 获取全部答题记录
+     * @return
+     */
     List<Stu_work> getAllStu_works();
 }
