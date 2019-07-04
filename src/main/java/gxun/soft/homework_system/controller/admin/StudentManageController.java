08be0366e23a289b4840a8e94ffa99f057493845
AronWiki <1639563006@qq.com>
2019-07-05 05:39:35 +0800

    更新管理端学生、教师controller

--- src/main/java/gxun/soft/homework_system/controller/admin/StudentManageController.java
+++ src/main/java/gxun/soft/homework_system/controller/admin/StudentManageController.java
@@ -1,17 +1,18 @@
 package gxun.soft.homework_system.controller.admin;
 
-import com.alibaba.fastjson.JSONArray;
-import gxun.soft.homework_system.component.ToJsonArray;
+import gxun.soft.homework_system.domain.Account;
 import gxun.soft.homework_system.domain.Student;
 import gxun.soft.homework_system.service.StudentService;
 import io.swagger.annotations.ApiOperation;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class StudentManageController {
@@ -19,7 +20,7 @@ public class StudentManageController {
     @Autowired
     private StudentService studentService;
 
-    @ApiOperation(value = "学生管理")
+    @ApiOperation(value = "学生列表")
     @GetMapping("/studentList")
     public String classManage(Model model){
         List<Student> studentList = studentService.getAllStudent();
@@ -27,4 +28,32 @@ public class StudentManageController {
         return "admin/studentList";
     }
 
+
+    @ApiOperation(value = "学生添加")
+    @GetMapping("/addStudent")
+    public String AddStudent(@Param("stuId") Integer stuId,
+                             @Param("stuName") String stuName,
+                             @Param("classId") Integer classId,
+                             @Param("password")String password,
+                             Model model){
+        Account account = new Account();
+        Student student = new Student();
+        Map studentMap = new HashMap();
+
+        account.setUserId(stuId);
+        account.setPassword(password);
+        account.setAccountType(0);
+
+        student.setStuId(stuId);
+        student.setStuName(stuName);
+        student.setClassId(classId);
+
+        studentMap.put("account",account);
+        studentMap.put("student",student);
+
+        studentService.addStudent(studentMap);
+        List<Student> studentList = studentService.getAllStudent();
+        model.addAttribute("studentList",studentList);
+        return "admin/studentList";
+    }
 }
--- src/main/java/gxun/soft/homework_system/controller/admin/TeacherManageController.java
+++ src/main/java/gxun/soft/homework_system/controller/admin/TeacherManageController.java
@@ -1,14 +1,20 @@
 package gxun.soft.homework_system.controller.admin;
 
+import gxun.soft.homework_system.domain.Account;
 import gxun.soft.homework_system.domain.Teacher;
 import gxun.soft.homework_system.service.TeacherService;
 import io.swagger.annotations.ApiOperation;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class TeacherManageController {
@@ -16,11 +22,78 @@ public class TeacherManageController {
     @Autowired
     private TeacherService teacherService;
 
-    @ApiOperation(value = "教师管理")
+    @ApiOperation(value = "教师列表")
     @GetMapping("/teacherList")
-    public String getClassList(Model model){
+    public String teacherList(Model model){
         List<Teacher> teacherList = teacherService.getAllTeacher();
         model.addAttribute("teacherList",teacherList);
         return "admin/teacherList";
     }
+
+    @ApiOperation(value = "教师添加")
+    @PostMapping("/addTeacher")
+    public String addTeacher(@Param("teaName") String teaName,
+                               @Param("teaId") Integer teaId,
+                               @Param("classId") Integer classId,
+                               @Param("password") String password,
+                               Model model){
+
+        Account account = new Account();
+        Teacher teacher = new Teacher();
+        Map teacherMap = new HashMap();
+
+        account.setUserId(teaId);
+        account.setPassword(password);
+        account.setAccountType(1);
+
+        teacher.setTeaId(teaId);
+        teacher.setClassId(classId);
+        teacher.setTeaName(teaName);
+
+        teacherMap.put("account",account);
+        teacherMap.put("teacher",teacher);
+        teacherService.addTeacher(teacherMap);
+
+        List<Teacher> teacherList = teacherService.getAllTeacher();
+        model.addAttribute("teacherList",teacherList);
+        return "admin/teacherList";
+    }
+
+    @ApiOperation(value = "教师查询")
+    @PostMapping("/findByTeacherName")
+    public String FindByTeacherName(@Param("teaName") String teaName,
+                                    Model model){
+        List<Teacher> teacherList = teacherService.findTeacherByTeacherName(teaName);
+        model.addAttribute("teacherList",teacherList);
+        return "admin/teacherList";
+    }
+
+    @ApiOperation(value = "教师删除")
+    @GetMapping("/deleteTeacher")
+    public String DeleteTeacher(@Param("teaId") Integer teaId,
+                                Model model){
+        teacherService.deleteTeacherByTeacherId(teaId);
+        List<Teacher> teacherList = teacherService.getAllTeacher();
+        model.addAttribute("teacherList",teacherList);
+        return "admin/teacherList";
+    }
+
+
+    @ApiOperation(value = "教师更新")
+    @PostMapping("/updateTeacherPassword")
+    public String UpdateTeacherPassword(@Param("teaId") Integer teaId,
+                                        @Param("password") String password,
+                                        Model model){
+        Account account = new Account();
+        account.setUserId(teaId);
+        account.setPassword(password);
+        teacherService.updateTeacherPassword(account);
+        List<Teacher> teacherList = teacherService.getAllTeacher();
+        model.addAttribute("teacherList",teacherList);
+        return "admin/teacherList";
+    }
+
+
+
+
 }
